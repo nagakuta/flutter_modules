2a6e9fbf1214f559a1e89b42c6345a3632229c62
nagakuta <nagakuta0314+github@gmail.com>
2022-06-05 21:34:50 +0900

    :heavy_plus_sign: Append analytics_service, crashlytics_service, crypto, firebase_auth, firebase_auth_platform_interface, firebase_service, flutter_facebook_auth, freezed_annotation, google_sign_in, mockito, riverpod, sign_in_with_apple, state_notifier, twitter_login, build_runner, and freezed

--- packages/auth_service/example/windows/flutter/generated_plugin_registrant.cc
+++ packages/auth_service/example/windows/flutter/generated_plugin_registrant.cc
@@ -7,8 +7,17 @@
 #include "generated_plugin_registrant.h"
 
 #include <auth_service/auth_service_plugin_c_api.h>
+#include <crashlytics_service/crashlytics_service_plugin_c_api.h>
+#include <firebase_service/firebase_service_plugin_c_api.h>
+#include <package_info_service/package_info_service_plugin_c_api.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
   AuthServicePluginCApiRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("AuthServicePluginCApi"));
+  CrashlyticsServicePluginCApiRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("CrashlyticsServicePluginCApi"));
+  FirebaseServicePluginCApiRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("FirebaseServicePluginCApi"));
+  PackageInfoServicePluginCApiRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("PackageInfoServicePluginCApi"));
 }
