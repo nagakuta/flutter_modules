fe1ea34bde354e1f8675b3bba1e925897537e434
nagakuta <nagakuta0314+github@gmail.com>
2022-06-06 15:03:52 +0900

    :sparkles: :clown_face: Implement sign-in methods

---
+++ packages/auth_service/lib/src/method/method.dart
@@ -0,0 +1,54 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'method.freezed.dart';
+
+/// AuthMethod
+@Freezed(map: FreezedMapOptions(maybeMap: false))
+class AuthMethod with _$AuthMethod {
+  // ignore: public_member_api_docs
+  const factory AuthMethod.anonymous() = AnonymousAuthMethod;
+
+  // ignore: public_member_api_docs
+  const factory AuthMethod.email({
+    required final String email,
+    required final String password,
+  }) = EmailAuthMethod;
+
+  // ignore: public_member_api_docs
+  const factory AuthMethod.google() = GoogleAuthMethod;
+
+  // ignore: public_member_api_docs
+  const factory AuthMethod.facebook() = FacebookAuthMethod;
+
+  // ignore: public_member_api_docs
+  const factory AuthMethod.apple() = AppleAuthMethod;
+
+  // ignore: public_member_api_docs
+  const factory AuthMethod.twitter({
+    required final String apiKey,
+    required final String apiSecretKey,
+    required final String redirectURI,
+  }) = TwitterAuthMethod;
+
+  // ignore: public_member_api_docs
+  const factory AuthMethod.phone({
+    required final String verificationId,
+    required final String smsCode,
+  }) = PhoneAuthMethod;
+
+  const AuthMethod._();
+}
+
+// ignore: public_member_api_docs
+extension AuthMethodExtension on AuthMethod {
+  /// メソッド名
+  String get name => map(
+        anonymous: (final _) => "anonymous",
+        email: (final _) => "email",
+        google: (final _) => "google",
+        facebook: (final _) => "facebook",
+        apple: (final _) => "apple",
+        twitter: (final _) => "twitter",
+        phone: (final _) => "phone",
+      );
+}
