07c878ea0cb46bcd3626f135db461779f3dd97d9
nagakuta <nagakuta0314+github@gmail.com>
2025-05-03 15:31:10 +0900

    ✨ Implement Organism widget

---
+++ packages/atomic_widget/lib/src/widgets/organism/_widget.dart
@@ -0,0 +1,6 @@
+import "package:flutter/widgets.dart";
+
+import '/src/_utils/wrapper/_widget.dart';
+
+part "stateful.dart";
+part "stateless.dart";
---
+++ packages/atomic_widget/lib/src/widgets/organism/stateful.dart
@@ -0,0 +1,26 @@
+part of "_widget.dart";
+
+/// Stateful Organism Widget
+abstract base class StatefulOrganismWidget extends StatefulWidget with StatefulWrapper {
+  // ignore: public_member_api_docs
+  const StatefulOrganismWidget({
+    super.key,
+    this.padding = EdgeInsets.zero,
+    this.margin = EdgeInsets.zero,
+  });
+
+  @override
+  @protected
+  final EdgeInsetsGeometry padding;
+
+  @override
+  @protected
+  final EdgeInsetsGeometry margin;
+
+  @override
+  @protected
+  OrganismState<StatefulOrganismWidget> createState();
+}
+
+/// State of [StatefulOrganismWidget]
+abstract base class OrganismState<T extends StatefulOrganismWidget> extends State<T> with WrapperState<T> {}
