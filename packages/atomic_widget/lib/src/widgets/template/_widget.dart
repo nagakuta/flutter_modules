cd73d54214090bd95b5aac4073fb574c4dc20dc5
nagakuta <nagakuta0314+github@gmail.com>
2025-05-03 17:19:27 +0900

    ✨ Implement Template widget

---
+++ packages/atomic_widget/lib/src/widgets/template/_widget.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/widgets.dart';
+
+import '/src/_utils/adaptive/types.dart';
+import '/src/_utils/adaptive/widget.dart';
+
+part 'layout.dart';
+part 'window.dart';
---
+++ packages/atomic_widget/lib/src/widgets/template/layout.dart
@@ -0,0 +1,66 @@
+part of '_widget.dart';
+
+/// Layout Template Widget
+abstract base class LayoutTemplateWidget extends SafeArea with AdaptiveWithLayoutType {
+  // ignore: public_member_api_docs
+  const LayoutTemplateWidget({
+    super.key,
+  }) : super(child: const SizedBox.shrink());
+
+  @override
+  bool get left => true;
+
+  @override
+  bool get top => true;
+
+  @override
+  bool get right => true;
+
+  @override
+  bool get bottom => true;
+
+  @override
+  EdgeInsets get minimum => EdgeInsets.zero;
+
+  @override
+  bool get maintainBottomViewPadding => false;
+
+  @override
+  Widget get child => LayoutBuilder(
+        builder: (
+          final BuildContext context,
+          final BoxConstraints constraints,
+        ) =>
+            switch (LayoutType.fromConstraints(constraints)) {
+          LayoutType.desktop => buildDesktop(context) ??
+              buildLargeTablet(context) ??
+              buildSmallTablet(context) ??
+              buildLargeHandset(context) ??
+              buildMediumHandset(context) ??
+              buildSmallHandset(context) ??
+              buildTemplate(context),
+          LayoutType.largeTablet => buildLargeTablet(context) ??
+              buildSmallTablet(context) ??
+              buildLargeHandset(context) ??
+              buildMediumHandset(context) ??
+              buildSmallHandset(context) ??
+              buildTemplate(context),
+          LayoutType.smallTablet => buildSmallTablet(context) ??
+              buildLargeHandset(context) ??
+              buildMediumHandset(context) ??
+              buildSmallHandset(context) ??
+              buildTemplate(context),
+          LayoutType.largeHandset => buildLargeHandset(context) ??
+              buildMediumHandset(context) ??
+              buildSmallHandset(context) ??
+              buildTemplate(context),
+          LayoutType.mediumHandset =>
+            buildMediumHandset(context) ?? buildSmallHandset(context) ?? buildTemplate(context),
+          LayoutType.smallHandset => buildSmallHandset(context) ?? buildTemplate(context),
+        },
+      );
+
+  /// Build a template widget.
+  @protected
+  Widget buildTemplate(final BuildContext context);
+}
