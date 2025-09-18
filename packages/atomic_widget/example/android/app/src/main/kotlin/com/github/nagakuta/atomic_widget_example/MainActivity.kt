95aca493d384e8b09fe200c466886bc571c43133
nagakuta <nagakuta0314+github@gmail.com>
2025-09-18 20:26:47 +0900

    ⬆️ Upgrade Flutter version to 3.32.8

--- packages/atomic_widget/example/android/app/src/main/kotlin/com/github/nagakuta/atomic_widget_example/MainActivity.kt
+++ packages/atomic_widget/example/android/app/src/main/kotlin/com/github/nagakuta/atomic_widget_example/MainActivity.kt
@@ -2,4 +2,4 @@ package com.github.nagakuta.atomic_widget_example
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity()
+class MainActivity : FlutterActivity()
